ad21eec7889d22ab805193511147c7418f2728f6
Ned Palacios <npdoesmc@gmail.com>
2024-01-28 19:44:47 +0800

    fix: use window origin as fallback url

--- frontend/src/chain.ts
+++ frontend/src/chain.ts
@@ -1,7 +1,7 @@
 // Using LangServe does not work as the chain input was not inferred correctly.
 // Instead, we use a custom chain implementation that uses fetch.
 const chain = (() => {
-    const url = import.meta.env.VITE_API_URL ?? "http://localhost:8080";
+    const url = import.meta.env.VITE_API_URL ?? window.location.origin;
 
     async function invoke(input: { input: string, history: {type: string, content: string}[] }) {
         const res = await fetch(new URL("/invoke", url), {
